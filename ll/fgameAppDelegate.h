b1b7c5d47b29a74a6b42e54405577996333c355a
yunfei <apple@ApplematoMacBook-Pro.local>
2012-01-23 20:09:38 +0800

    Initial Commit

---
+++ ll/fgameAppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  fgameAppDelegate.h
+//  ll
+//
+//  Created by Apple on 12-1-23.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface fgameAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
---
+++ llTests/llTests.h
@@ -0,0 +1,13 @@
+//
+//  llTests.h
+//  llTests
+//
+//  Created by Apple on 12-1-23.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface llTests : SenTestCase
+
+@end
